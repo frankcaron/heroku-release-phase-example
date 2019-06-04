37da4b91259308d9e71b4dec03e412a4f8ae8342
Frank Caron <frank@frankcaron.com>
2019-06-04 13:38:24 -0400

    Added AWS, ejs var for base url

--- index.js
+++ index.js
@@ -8,5 +8,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/index', {
+    baseUrl: AMAZON_S3_BASE_URL
+  }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
