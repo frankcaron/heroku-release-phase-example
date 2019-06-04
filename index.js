c268240908a8cb3788dbc4042bb60ced2ba7567e
Frank Caron <frank@frankcaron.com>
2019-06-04 10:51:57 -0400

    Added release phase and env for S3

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const AMAZON_S3_BASE_URL = process.env.AMAZON_S3_BASE_URL || ''
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
